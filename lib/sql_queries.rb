f6a004de23cb80c9ec1efdf2a8b0776b143e9a93
Muh Syaiful Romadhon <sngeblog@gmail.com>
2019-03-28 21:05:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-  
+  "SELECT projects.title, SUM(amount) FROM pledges INNER JOIN projects ON pledges.project_id = projects.id GROUP BY projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
