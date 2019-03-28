ee470e5781a95515d0127f431f129bd757698abf
Muh Syaiful Romadhon <sngeblog@gmail.com>
2019-03-28 20:57:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,6 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
   "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
   FROM projects
   INNER JOIN pledges
@@ -28,7 +27,6 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
   "SELECT users.name, SUM(pledges.amount)
   FROM users
   INNER JOIN pledges
@@ -38,7 +36,6 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
   "SELECT projects.category, pledges.amount
   FROM projects
   INNER JOIN pledges
@@ -47,7 +44,6 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
   "SELECT projects.category, SUM(pledges.amount)
   FROM projects
   INNER JOIN pledges
