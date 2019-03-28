08fd0b1ab4be7bc841314c8581db07c784e8ebd4
Muh Syaiful Romadhon <sngeblog@gmail.com>
2019-03-28 21:06:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,6 +19,7 @@ INSERT INTO users (id, name, age) VALUES
 (18, 'Pacha', 5),
 (19, 'Ena', 24),
 (20, 'Katie', 24);
+
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 (1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
 (2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
